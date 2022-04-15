646e947d5735744f49127d336013e1d1a3360a34
Michelepal <Mikaelquasimodus@gmail.com>
2022-04-15 11:37:54 +0200

    creazione e impostazione generale header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigazioneComponent } from './header/navigazione.component';
+import { TitoloComponent } from './header/titolo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigazioneComponent,
+    TitoloComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/navigazione.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavigazioneComponent } from './navigazione.component';
+
+describe('NavigazioneComponent', () => {
+  let component: NavigazioneComponent;
+  let fixture: ComponentFixture<NavigazioneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavigazioneComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigazioneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
