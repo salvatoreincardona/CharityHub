4a3039e42569d7c92d45dd7546af5f1d18a2d742
Michelepal <Mikaelquasimodus@gmail.com>
2022-04-15 12:00:13 +0200

    Update app-routing.module.ts
    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
-  { path: '/home', component: HomeComponent },
-  { path: '/info', component: InfoComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
   { path: '**', component: HomeComponent }
 ];
 
